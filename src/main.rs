5f9d231daf341cce92101c75d91eef029c7bc744
pasmophobia <l@leonmatsuistroemsvik.com>
2025-01-16 14:04:56 +0900

    feat: 宛先をfromではなくenvelopeで判定するように変更

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod models;
 
 use crate::config::ConfigRoot;
 use actix_multipart::Multipart;
-use actix_web::web::{Buf, Bytes, BytesMut};
+use actix_web::web::{Bytes, BytesMut};
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use confy::ConfyError;
 use env_logger::{Builder, Target};
@@ -14,6 +14,7 @@ use serenity::http::Http;
 use serenity::model::webhook::Webhook;
 use std::collections::HashMap;
 use std::str::FromStr;
+use crate::models::Envelope;
 
 const HELLO: [&str; 4] = [
     "       ___     __",
@@ -68,13 +69,18 @@ async fn listen(_config: &ConfigRoot) -> std::io::Result<()> {
     .await
 }
 
+fn bad_request(message: &str) -> HttpResponse {
+    trace!("Bad request: {}", message);
+    HttpResponse::BadRequest().body(message.to_string())
+}
+
 async fn transfer(
     config: web::Data<ConfigRoot>,
     query: web::Query<models::QueryParameters>,
     mut body: Multipart,
 ) -> impl Responder {
     let passphrase = &config.server.passphrase;
-    if (&query.passphrase != passphrase) {
+    if &query.passphrase != passphrase {
         return HttpResponse::BadRequest().body("Invalid passphrase");
     }
 
@@ -95,12 +101,12 @@ async fn transfer(
                     trace!("Received multipart file");
                     let content_disposition = match field.content_disposition() {
                         None => {
-                            return HttpResponse::BadRequest().body("Missing content disposition")
+                            return bad_request("Missing content disposition")
                         }
                         Some(content_disposition) => content_disposition,
                     };
                     let filename = match content_disposition.get_filename() {
-                        None => return HttpResponse::BadRequest().body("Missing filename"),
+                        None => return bad_request("Missing filename"),
                         Some(filename) => filename.to_string(),
                     };
                     let mut bytes = BytesMut::new();
@@ -108,7 +114,7 @@ async fn transfer(
                         let chunk = match chunk {
                             Ok(chunk) => chunk,
                             Err(_) => {
-                                return HttpResponse::BadRequest().body("Failed to read chunk")
+                                return bad_request("Failed to read chunk")
                             }
                         };
                         bytes.extend_from_slice(chunk.clone().as_ref());
@@ -120,20 +126,20 @@ async fn transfer(
                 // form-data
                 let name = match field.name() {
                     Some(name) => name.to_string(),
-                    None => return HttpResponse::BadRequest().body("Missing name"),
+                    None => return bad_request("Missing name"),
                 };
 
                 let mut bytes = BytesMut::new();
                 while let Some(chunk) = field.next().await {
                     let chunk = match chunk {
                         Ok(chunk) => chunk,
-                        Err(_) => return HttpResponse::BadRequest().body("Failed to read chunk"),
+                        Err(_) => return bad_request("Failed to read chunk"),
                     };
                     bytes.extend_from_slice(chunk.clone().as_ref());
                 }
                 let data = match std::str::from_utf8(bytes.as_ref()) {
                     Ok(data) => data,
-                    Err(_) => return HttpResponse::BadRequest().body("Failed to read data"),
+                    Err(_) => return bad_request("Failed to read data"),
                 };
                 form_data.insert(name.clone(), data.to_string());
             }
@@ -144,42 +150,51 @@ async fn transfer(
 
     debug!("Sending webhook request...");
     let http = Http::new("");
-    let webhook_url = match config.webhook.url.get(form_data["to"].as_str()) {
-        Some(url) => url,
-        None => return HttpResponse::BadRequest().body("Missing to"),
-    };
-    let webhook = match Webhook::from_url(&http, webhook_url).await {
-        Ok(webhook) => webhook,
-        Err(_) => return HttpResponse::BadRequest().body("Invalid webhook"),
-    };
 
-    let builder = ExecuteWebhook::new()
-        .username(config.webhook.username.clone())
-        .avatar_url(config.webhook.avatar_url.clone())
-        .embed(
-            CreateEmbed::new()
-                .title(config.webhook.title.clone())
-                .field(config.webhook.from.clone(), form_data["from"].clone(), true)
-                .field(config.webhook.to.clone(), form_data["to"].clone(), true)
-                .field(
-                    config.webhook.subject.clone(),
-                    form_data["subject"].clone(),
-                    true,
-                )
-                .field(
-                    config.webhook.text.clone(),
-                    form_data["text"].clone(),
-                    false,
-                ),
-        )
-        .add_files(
-            files
-                .iter()
-                .map(|(name, bytes)| CreateAttachment::bytes(bytes.to_vec(), name.clone())),
-        );
 
-    match webhook.execute(&http, false, builder).await {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::BadRequest().body("Failed to execute webhook"),
+    let envelope = match serde_json::from_str::<Envelope>(form_data["envelope"].as_str()) {
+        Ok(envelope) => envelope,
+        Err(_) => return bad_request("Failed to parse envelope"),
+    };
+    for to in envelope.to {
+        let webhook_url = match config.webhook.url.get(to.as_str()) {
+            Some(url) => url,
+            None => return bad_request("Missing to"),
+        };
+        let webhook = match Webhook::from_url(&http, webhook_url).await {
+            Ok(webhook) => webhook,
+            Err(_) => return bad_request("Invalid webhook"),
+        };
+
+        let builder = ExecuteWebhook::new()
+            .username(config.webhook.username.clone())
+            .avatar_url(config.webhook.avatar_url.clone())
+            .embed(
+                CreateEmbed::new()
+                    .title(config.webhook.title.clone())
+                    .field(config.webhook.from.clone(), form_data["from"].clone(), true)
+                    .field(config.webhook.to.clone(), form_data["to"].clone(), true)
+                    .field(
+                        config.webhook.subject.clone(),
+                        form_data["subject"].clone(),
+                        true,
+                    )
+                    .field(
+                        config.webhook.text.clone(),
+                        form_data["text"].clone(),
+                        false,
+                    ),
+            )
+            .add_files(
+                files
+                    .iter()
+                    .map(|(name, bytes)| CreateAttachment::bytes(bytes.to_vec(), name.clone())),
+            );
+
+        match webhook.execute(&http, false, builder).await  {
+            Err(_) => return bad_request("Failed to execute webhook"),
+            Ok(_) => {}
+        }
     }
+    HttpResponse::Ok().into()
 }
--- src/models.rs
+++ src/models.rs
@@ -1,33 +1,9 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
-pub struct Mail {
-    pub headers: String, //
-    pub dkim: String,    //
-    pub to: String,      //
-    pub text: String,    //
-    pub html: Option<String>,
-    pub from: String,      //
-    pub sender_ip: String, //
-    pub spam_report: Option<String>,
-    pub envelope: String,    //
-    pub attachments: String, //
-    pub subject: String,     //
-    pub spam_score: Option<u32>,
-    #[serde(rename = "attachment-info")]
-    pub attachment_info: Option<AttachmentInfo>,
-    pub charsets: String, //
-    #[serde(rename = "SPF")]
-    pub spf: String, //
-}
-
-#[derive(Deserialize)]
-pub struct AttachmentInfo {
-    pub filename: String,
-    #[serde(rename = "type")]
-    pub _type: String,
-    #[serde(rename = "content-id")]
-    pub content_id: u32,
+#[derive(Serialize, Deserialize)]
+pub struct Envelope {
+    pub to: Vec<String>,
+    pub from: String,
 }
 
 #[derive(Deserialize)]
